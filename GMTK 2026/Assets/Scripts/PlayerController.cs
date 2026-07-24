1cb631eee421fc90283da96411d7045f299992e0
RyanDeBarros <ryan2004db@gmail.com>
2026-07-23 22:57:48 -0400

    connect input actions to player controllers

---
+++ GMTK 2026/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Shoot()
+    {
+        Debug.Log(name + ": Shoot");
+        // TODO
+    }
+
+    public void Reload()
+    {
+        Debug.Log(name + ": Reload");
+        // TODO
+    }
+
+    public void Dodge()
+    {
+        Debug.Log(name + ": Dodge");
+        // TODO
+    }
+}
