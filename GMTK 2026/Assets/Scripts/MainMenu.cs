94747722bbf99ad15f109cc7152860bae362334d
Ryan de Barros <121144814+RyanDeBarros@users.noreply.github.com>
2026-07-26 13:56:59 -0400

    Revert "Match complete redo"

--- GMTK 2026/Assets/Scripts/MainMenu.cs
+++ GMTK 2026/Assets/Scripts/MainMenu.cs
@@ -3,16 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private void Start()
-    {
-        Soundtrack.Play(Song.MainMenu);
-    }
-
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
     }
-
     public void QuitGame()
     {
         Application.Quit();
