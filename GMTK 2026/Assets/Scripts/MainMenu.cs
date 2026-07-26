b0db8d8d4930bc9710288e24f1b67311634c750f
saraludus <saraloudagh@gmail.com>
2026-07-26 14:43:35 -0400

    fixed scaling for match complete screen

--- GMTK 2026/Assets/Scripts/MainMenu.cs
+++ GMTK 2026/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
+        Soundtrack.Play(Song.MainMenu);
     }
     public void QuitGame()
     {
